4c76df11f9248fc7ebc133b64d99e5da7a490a51
TocDx <sk372021000@gmail.com>
2023-12-11 15:05:04 +0900

    [Feat] hook,page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
-import styled from 'styled-components';
-import Banner from './components/Banner';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+import SearchPage from './pages/SearchPage';
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <Container>
-      <Nav />
-      <Banner />
-    </Container>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="main" element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
 export default App;
-
-const Container = styled.main`
-  position: relative;
-  min-height: calc(100vh - 250px);
-  overflow-x: hidden;
-  display: block;
-  top: 72px;
-  padding: 0 calc(3.5vw + 5px);
-
-  &:after {
-    background: url('/images/home-background.png') center center / cover
-      no-repeat fixed;
-    content: '';
-    position: absolute;
-    inset: 0px;
-    opacity: 1;
-    z-index: -1;
-  }
-`;
